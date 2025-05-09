c3e26bebc5f5f11a71b738b9e84dd6393e532d30
kalyan <kalyankatwal2@gmail.com>
2025-05-09 08:57:59 +0545

    Food App Initialization

---
+++ src/test/java/com/kalyan/myfood/MyfoodApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kalyan.myfood;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyfoodApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
